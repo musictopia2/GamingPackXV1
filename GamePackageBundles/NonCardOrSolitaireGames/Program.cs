2750da6665be0cce4e059473d318ea4cc0fb8407
Andy <musictopia2@hotmail.com>
2020-12-10 21:07:52 -0700

    Single Player Loaders Again. Hopefully has pwa now.

---
+++ GamePackageBundles/NonCardOrSolitaireGames/Program.cs
@@ -0,0 +1,16 @@
+using GameLoaderBlazorLibrary;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using System.Threading.Tasks;
+namespace NonCardOrSolitaireGames
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+            builder.Services.RegisterDefaultSinglePlayerProcesses<BasicViewModel>();
+            await builder.Build().RunAsync();
+        }
+    }
+}
