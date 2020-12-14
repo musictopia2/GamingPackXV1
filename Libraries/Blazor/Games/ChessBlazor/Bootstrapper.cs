874560e868dcc74e8e107394dfd9918710418fe1
Andy <musictopia2@hotmail.com>
2020-12-14 14:23:28 -0700

    Chinese Checkers Changes Completed.

--- Libraries/Blazor/Games/ChessBlazor/Bootstrapper.cs
+++ Libraries/Blazor/Games/ChessBlazor/Bootstrapper.cs
@@ -1,26 +1,10 @@
-using System;
-using System.Text;
-using CommonBasicStandardLibraries.Exceptions;
-using CommonBasicStandardLibraries.AdvancedGeneralFunctionsAndProcesses.BasicExtensions;
-using System.Linq;
-using CommonBasicStandardLibraries.BasicDataSettingsAndProcesses;
-using static CommonBasicStandardLibraries.BasicDataSettingsAndProcesses.BasicDataFunctions;
-using CommonBasicStandardLibraries.CollectionClasses;
-using System.Threading.Tasks; //most of the time, i will be using asyncs.
-using fs = CommonBasicStandardLibraries.AdvancedGeneralFunctionsAndProcesses.JsonSerializers.FileHelpers;
-using js = CommonBasicStandardLibraries.AdvancedGeneralFunctionsAndProcesses.JsonSerializers.NewtonJsonStrings; //just in case i need those 2.
-using ChessCP.ViewModels;
-using BasicGameFrameworkLibrary.CommonInterfaces;
 using BasicGameFrameworkLibrary.BasicGameDataClasses;
+using BasicGameFrameworkLibrary.CommonInterfaces;
 using BasicGameFrameworkLibrary.StandardImplementations.GlobalClasses;
 using BasicGamingUIBlazorLibrary.Bootstrappers;
-using BasicGameFrameworkLibrary.MultiplayerClasses.LoadingClasses;
 using ChessCP.Data;
-using BasicGameFrameworkLibrary.MiscProcesses;
-using BasicGameFrameworkLibrary.ViewModels;
-using BasicGameFrameworkLibrary.MultiplayerClasses.MiscHelpers;
-using BasicGameFrameworkLibrary.MultiplayerClasses.BasicGameClasses;
-//i think this is the most common things i like to do
+using ChessCP.ViewModels;
+using System.Threading.Tasks; //most of the time, i will be using asyncs.
 namespace ChessBlazor
 {
     public class Bootstrapper : MultiplayerBasicBootstrapper<ChessShellViewModel>
@@ -28,37 +12,11 @@ namespace ChessBlazor
         public Bootstrapper(IStartUp starts, EnumGamePackageMode mode) : base(starts, mode)
         {
         }
-
         protected override Task ConfigureAsync()
         {
             OurContainer!.RegisterNonSavedClasses<ChessShellViewModel>();
-            OurContainer!.RegisterType<BasicGameLoader<ChessPlayerItem, ChessSaveInfo>>();
-            OurContainer.RegisterType<RetrieveSavedPlayers<ChessPlayerItem, ChessSaveInfo>>();
-            OurContainer.RegisterType<MultiplayerOpeningViewModel<ChessPlayerItem>>(true); //had to be set to true after all.
-            //anything that needs to be registered will be here.
-
-
-            OurContainer.RegisterType <BeginningColorProcessorClass<EnumColorChoice, ChessPlayerItem, ChessSaveInfo>>();
-            OurContainer.RegisterType<BeginningChooseColorViewModel<EnumColorChoice, ChessPlayerItem>>();
-            OurContainer.RegisterType<BeginningColorModel<EnumColorChoice, ChessPlayerItem>>();
-
-            //good news is even if using a different item like pawn piece, won't affect this part (good decision).
-
-
-
-            //i think this is still needed (?)
-            MiscDelegates.GetMiscObjectsToReplace = (() =>
-            {
-                CustomBasicList<Type> output = new CustomBasicList<Type>()
-                {
-                    typeof(BeginningColorProcessorClass<EnumColorChoice, ChessPlayerItem, ChessSaveInfo>),
-                    typeof(BeginningColorModel<EnumColorChoice, ChessPlayerItem>)
-                    //typeof(BeginningColorProcessorClass<EnumColorChoice, CheckerChoiceCP<EnumColorChoice>, ChessPlayerItem, ChessSaveInfo>),
-                    //typeof(BeginningColorModel<EnumColorChoice, CheckerChoiceCP<EnumColorChoice>, ChessPlayerItem>)
-                };
-                return output;
-            });
-
+            OurContainer.RegisterCommonMultplayerClasses<ChessPlayerItem, ChessSaveInfo>();
+            OurContainer.RegisterBeginningColors<EnumColorChoice, ChessPlayerItem, ChessSaveInfo>();
             return Task.CompletedTask;
         }
     }
--- Libraries/Blazor/Games/ChineseCheckersBlazor/Bootstrapper.cs
+++ Libraries/Blazor/Games/ChineseCheckersBlazor/Bootstrapper.cs
@@ -1,26 +1,10 @@
-using System;
-using System.Text;
-using CommonBasicStandardLibraries.Exceptions;
-using CommonBasicStandardLibraries.AdvancedGeneralFunctionsAndProcesses.BasicExtensions;
-using System.Linq;
-using CommonBasicStandardLibraries.BasicDataSettingsAndProcesses;
-using static CommonBasicStandardLibraries.BasicDataSettingsAndProcesses.BasicDataFunctions;
-using CommonBasicStandardLibraries.CollectionClasses;
-using System.Threading.Tasks; //most of the time, i will be using asyncs.
-using fs = CommonBasicStandardLibraries.AdvancedGeneralFunctionsAndProcesses.JsonSerializers.FileHelpers;
-using js = CommonBasicStandardLibraries.AdvancedGeneralFunctionsAndProcesses.JsonSerializers.NewtonJsonStrings; //just in case i need those 2.
-using ChineseCheckersCP.ViewModels;
-using BasicGameFrameworkLibrary.CommonInterfaces;
 using BasicGameFrameworkLibrary.BasicGameDataClasses;
+using BasicGameFrameworkLibrary.CommonInterfaces;
 using BasicGameFrameworkLibrary.StandardImplementations.GlobalClasses;
 using BasicGamingUIBlazorLibrary.Bootstrappers;
-using BasicGameFrameworkLibrary.MultiplayerClasses.LoadingClasses;
 using ChineseCheckersCP.Data;
-using BasicGameFrameworkLibrary.MiscProcesses;
-using BasicGameFrameworkLibrary.ViewModels;
-using BasicGameFrameworkLibrary.MultiplayerClasses.MiscHelpers;
-using BasicGameFrameworkLibrary.MultiplayerClasses.BasicGameClasses;
-//i think this is the most common things i like to do
+using ChineseCheckersCP.ViewModels;
+using System.Threading.Tasks; //most of the time, i will be using asyncs.
 namespace ChineseCheckersBlazor
 {
     public class Bootstrapper : MultiplayerBasicBootstrapper<ChineseCheckersShellViewModel>
@@ -28,36 +12,11 @@ namespace ChineseCheckersBlazor
         public Bootstrapper(IStartUp starts, EnumGamePackageMode mode) : base(starts, mode)
         {
         }
-
         protected override Task ConfigureAsync()
         {
             OurContainer!.RegisterNonSavedClasses<ChineseCheckersShellViewModel>();
-            OurContainer!.RegisterType<BasicGameLoader<ChineseCheckersPlayerItem, ChineseCheckersSaveInfo>>();
-            OurContainer.RegisterType<RetrieveSavedPlayers<ChineseCheckersPlayerItem, ChineseCheckersSaveInfo>>();
-            OurContainer.RegisterType<MultiplayerOpeningViewModel<ChineseCheckersPlayerItem>>(true); //had to be set to true after all.
-            //anything that needs to be registered will be here.
-
-
-            OurContainer.RegisterType <BeginningColorProcessorClass<EnumColorChoice, ChineseCheckersPlayerItem, ChineseCheckersSaveInfo>>();
-            OurContainer.RegisterType<BeginningChooseColorViewModel<EnumColorChoice, ChineseCheckersPlayerItem>>();
-            OurContainer.RegisterType<BeginningColorModel<EnumColorChoice, ChineseCheckersPlayerItem>>();
-
-            //good news is even if using a different item like pawn piece, won't affect this part (good decision).
-
-
-
-            //i think this is still needed (?)
-            MiscDelegates.GetMiscObjectsToReplace = (() =>
-            {
-                CustomBasicList<Type> output = new CustomBasicList<Type>()
-                {
-                    typeof(BeginningColorProcessorClass<EnumColorChoice, ChineseCheckersPlayerItem, ChineseCheckersSaveInfo>),
-                    typeof(BeginningColorModel<EnumColorChoice, ChineseCheckersPlayerItem>)
-                    //typeof(BeginningColorProcessorClass<EnumColorChoice, CheckerChoiceCP<EnumColorChoice>, ChineseCheckersPlayerItem, ChineseCheckersSaveInfo>),
-                    //typeof(BeginningColorModel<EnumColorChoice, CheckerChoiceCP<EnumColorChoice>, ChineseCheckersPlayerItem>)
-                };
-                return output;
-            });
+            OurContainer.RegisterCommonMultplayerClasses<ChineseCheckersPlayerItem, ChineseCheckersSaveInfo>();
+            OurContainer.RegisterBeginningColors<EnumColorChoice, ChineseCheckersPlayerItem, ChineseCheckersSaveInfo>();
 
             return Task.CompletedTask;
         }
